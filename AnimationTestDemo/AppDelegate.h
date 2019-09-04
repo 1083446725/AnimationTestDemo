ac7f7b6a0b9f01c0b0e64485e726a853031016ad
郭冬冬 <guodongdong@inke.cn>
2019-09-04 10:28:58 +0800

    Initial Commit

---
+++ AnimationTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTestDemo
+//
+//  Created by guodongdong on 2019/9/4.
+//  Copyright © 2019 guodongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
